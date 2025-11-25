5d226843d5827258e640f839de0bb8274f192945
yvvonne <yvonnekajuju6@gmail.com>
2025-11-25 09:16:03 +0300

    final commit

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
+def return_evens(sequence):
+    return [n for n in sequence if n % 2 == 0]
 
-def make_exclamation(sentence_list):
-    pass
+
+def make_exclamation(sentences):
+    return [sentence + "!" for sentence in sentences]
